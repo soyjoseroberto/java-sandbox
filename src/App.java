ec48043640c6b1176ddb716324b3dc225923efd5
Jose R. Rodriguez <jrodriguez18@gmail.com>
2024-08-15 20:53:38 -0400

    One value to multiple variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String firstName = "Jose";
+        String lastName = "Rodriguez";
+        System.out.println(firstName + lastName);
+
+        // One value to multiple variables
+        int x, y, z;
+        x = y = z= 50;
+        System.out.println(x + y + z);
     }
 }
