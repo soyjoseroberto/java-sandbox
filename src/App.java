e2aca05cfff5922cf015b1798a359d2e04877391
Jose R. Rodriguez <jrodriguez18@gmail.com>
2024-08-16 22:32:34 -0400

    Added a Person class

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -9,5 +10,8 @@ public class App {
         int x, y, z;
         x = y = z= 50;
         System.out.println(x + y + z);
+
+        Person person = new Person("Jose", "Rodriguez", 41);
+        System.out.printf("My name is %s!", person.firstName);
     }
 }
